a408dae6fe1f50bbc32772486bf8f66d9af506c7
Pablo Penalva <penalva.pablo@gmail.com>
2022-11-04 11:21:24 -0300

    homepage spanish finished

--- components/Contacto/Contacto.js
+++ components/Contacto/Contacto.js
@@ -8,17 +8,23 @@ import { faWhatsapp, faInstagram, faFacebookF } from '@fortawesome/free-brands-s
 const Contacto = () => {
   return (
     <section id="Contact" className={styles.contacto}>
-      <Image src={logo} alt='logo-polo-&-wine' className={styles.img} />
-      <h2 className={styles.title}>Contacto</h2>
-      <p className={styles.subTitle}>Teléfono</p>
-      <p className={styles.text}>+447397793899</p>
-      <p className={styles.subTitle}>Email</p>
-      <p className={styles.text}>info@poloandwine.com</p>
-      <p className={styles.subTitle}>Mis redes sociales</p>
-      <div className={styles.SocialLinksBox}>
-        <a href='https://wa.link/jdmx80' rel='noreferrer' target="_blank">  <FontAwesomeIcon icon={faWhatsapp} /></a>
-        <a href='https://instagram.com/poloandwine' rel='noreferrer' target={'_blank'}><FontAwesomeIcon icon={faInstagram} /></a>
-        <a href='https://www.facebook.com/soledadsaltoconsulting' rel='noreferrer' target={'_blank'}><FontAwesomeIcon icon={faFacebookF} /></a>
+      <div className={styles.desktopContainer}>
+        <div className={styles.container1}>
+          <Image src={logo} alt='logo-polo-&-wine' className={styles.img} />
+          {/* <h2 className={styles.title}>Contacto</h2> */}
+        </div>
+        <div className={styles.container2}>
+          <p className={styles.subTitle}>Teléfono</p>
+          <p className={styles.text}>+447397793899</p>
+          <p className={styles.subTitle}>Email</p>
+          <p className={styles.text}>info@poloandwine.com</p>
+          <p className={styles.subTitle}>Mis redes sociales</p>
+          <div className={styles.SocialLinksBox}>
+            <a href='https://wa.link/jdmx80' rel='noreferrer' target="_blank">  <FontAwesomeIcon icon={faWhatsapp} /></a>
+            <a href='https://instagram.com/poloandwine' rel='noreferrer' target={'_blank'}><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href='https://www.facebook.com/soledadsaltoconsulting' rel='noreferrer' target={'_blank'}><FontAwesomeIcon icon={faFacebookF} /></a>
+          </div>
+        </div>
       </div>
       <p className={styles.copyright}><span>Polo & Wine.</span> Todos los derechos reservado 2022</p>
     </section>
--- components/homepage/Numeros/Numeros.js
+++ components/homepage/Numeros/Numeros.js
@@ -9,30 +9,34 @@ const Numeros = ({ languages }) => {
     <section className={styles.section}>
       <h2 className={styles.title}>Nuestros Números</h2>
       <div className={styles.container}>
-        <div className={`${styles.item} ${styles.item_1}`}>
-          <FontAwesomeIcon icon={faFaceSmileBeam} className={styles.itemIcon} />
-          <p className={styles.itemNumber}>250</p>
-          <p className={styles.itemText}>Pasajeros felices</p>
+        <div className={styles.desktopContainer1}>
+          <div className={`${styles.item} ${styles.item_1}`}>
+            <FontAwesomeIcon icon={faFaceSmileBeam} className={styles.itemIcon} />
+            <p className={styles.itemNumber}>250</p>
+            <p className={styles.itemText}>Pasajeros felices</p>
+          </div>
+          <div className={`${styles.item} ${styles.item_2}`}>
+            <FontAwesomeIcon icon={faArrowUpRightDots} className={styles.itemIcon} />
+            <p className={styles.itemNumber}>15</p>
+            <p className={styles.itemText}>Años de trayectoria</p>
+          </div>
         </div>
-        <div className={`${styles.item} ${styles.item_2}`}>
-          <FontAwesomeIcon icon={faArrowUpRightDots} className={styles.itemIcon} />
-          <p className={styles.itemNumber}>15</p>
-          <p className={styles.itemText}>Años de trayectoria</p>
-        </div>
-        <div className={`${styles.item} ${styles.item_3}`}>
-          <FontAwesomeIcon icon={faSoccerBall} className={styles.itemIcon} />
-          <p className={styles.itemNumber}>3</p>
-          <p className={styles.itemText}>Viajes a mundiales <br /> de futbol</p>
-        </div>
-        <div className={`${styles.item} ${styles.item_4}`}>
-          <FontAwesomeIcon icon={faTruckPlane} className={styles.itemIcon} />
-          <p className={styles.itemNumber}>38</p>
-          <p className={styles.itemText}>Tours y actividades<br /> en destino</p>
-        </div>
-        <div className={`${styles.item} ${styles.item_5}`}>
-          <FontAwesomeIcon icon={faHandshake} className={styles.itemIcon} />
-          <p className={styles.itemNumber}>7</p>
-          <p className={styles.itemText}>Partners y operadores turísticos asociados</p>
+        <div className={styles.desktopContainer2}>
+          <div className={`${styles.item} ${styles.item_3}`}>
+            <FontAwesomeIcon icon={faSoccerBall} className={styles.itemIcon} />
+            <p className={styles.itemNumber}>3</p>
+            <p className={styles.itemText}>Viajes a mundiales <br /> de futbol</p>
+          </div>
+          <div className={`${styles.item} ${styles.item_4}`}>
+            <FontAwesomeIcon icon={faTruckPlane} className={styles.itemIcon} />
+            <p className={styles.itemNumber}>38</p>
+            <p className={styles.itemText}>Tours y actividades<br /> en destino</p>
+          </div>
+          <div className={`${styles.item} ${styles.item_5}`}>
+            <FontAwesomeIcon icon={faHandshake} className={styles.itemIcon} />
+            <p className={styles.itemNumber}>7</p>
+            <p className={styles.itemText}>Partners y operadores turísticos asociados</p>
+          </div>
         </div>
       </div>
 
