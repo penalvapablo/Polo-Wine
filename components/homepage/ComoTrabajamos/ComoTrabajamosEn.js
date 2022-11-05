d3f339c0ad224188d35978eeb1303608c7634dd9
Pablo Penalva <penalva.pablo@gmail.com>
2022-11-05 09:58:00 -0300

    fixes sole 2

--- components/homepage/ComoTrabajamos/ComoTrabajamosEn.js
+++ components/homepage/ComoTrabajamos/ComoTrabajamosEn.js
@@ -4,7 +4,7 @@ const ComoTrabajamosEn = () => {
   return (
     <section id='ComoTrabajamos' className={styles.section}>
       <h2 className={styles.title}>How do we work?</h2>
-      <h3 className={styles.subTitle}>How to book in Polo & Wine?</h3>
+      <h3 className={styles.subTitle}>How to book with us?</h3>
       <p className={styles.text}>The first step is to choose your package. We have different options available for each event and we adapt them to your needs and requirements.
         <br /><br />The second step is to choose your hotel. We have a wide range of hotels around the world and can advise you on location and rates.
         <br /><br />
