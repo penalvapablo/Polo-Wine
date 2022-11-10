024279dd8a4c8ad3b89acc09b4ce7924146fb8e4
Pablo Penalva <penalva.pablo@gmail.com>
2022-11-10 17:02:48 -0300

    video added

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config, { isServer }) {
+    const prefix = config.assetPrefix ?? config.basePath ?? '';
+    config.module.rules.push({
+      test: /\.mp4$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          publicPath: `${prefix}/_next/static/media/`,
+          outputPath: `${isServer ? '../' : ''}static/media/`,
+          name: '[name].[hash].[ext]',
+        },
+      }],
+    });
+
+    return config;
+  },
 }
+
+
 
 module.exports = nextConfig
