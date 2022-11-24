b9f04a47977d52b15295cf044aeedf63c7dd5fd6
Pablo Penalva <penalva.pablo@gmail.com>
2022-11-24 16:49:05 -0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import AppContext from '../components/AppContext'
 import { useState } from 'react'
+import Script from 'next/script'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
@@ -15,9 +16,21 @@ function MyApp({ Component, pageProps }) {
     setLanguage('en')
   }
   return (
-    <AppContext.Provider value={{ language, languageFnEs, languageFnEn }}>
-      <Component {...pageProps} />
-    </AppContext.Provider >
+    <>
+      <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy='lazyOnload' id='google-tag'>
+        {
+          `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`
+        }
+      </Script>
+      <AppContext.Provider value={{ language, languageFnEs, languageFnEn }}>
+        <Component {...pageProps} />
+      </AppContext.Provider >
+    </>
   )
 }
 
