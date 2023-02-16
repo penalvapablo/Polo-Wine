3e4d7bebd03c7a2669ffd6017f9955a7d0c71796
Pablo Penalva <penalva.pablo@gmail.com>
2023-02-16 13:22:03 -0300

    nav update

---
+++ components/Nav/NavEs.js
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import logo from '/public/logo-polo-and-wine.png';
+import styles from './Nav.module.scss';
+import { useRouter } from 'next/router';
+
+const NavEs = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <div className={styles.nav}>
+      <Image
+        src={logo}
+        alt="logo-polo-&-wine"
+        className={styles.logo}
+      />
+      <nav className={styles.navLinks}>
+        <ul>
+          <li>
+            <Link
+              href="/es/"
+              className={`${
+                pathname === '/es' && styles.active
+              }`}>
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/events"
+              className={`${
+                pathname === '/events' && styles.active
+              }`}>
+              Eventos
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className={`${
+                pathname === '/about' && styles.active
+              }`}>
+              Quien soy
+            </Link>
+          </li>
+          <li>
+            <Link href={'#Contact'}>Contacto</Link>
+          </li>
+
+          <div className={styles.languageBtns}>
+            <Link
+              className={styles.languageBtn}
+              href="/es/">
+              ES
+            </Link>
+            <p>/</p>
+            <Link
+              className={styles.languageBtn}
+              href="/">
+              EN
+            </Link>
+          </div>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavEs;
--- pages/es/index.js
+++ pages/es/index.js
@@ -1,20 +1,21 @@
-import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
 // import { useContext } from "react";
 // import AppContext from '/components/AppContext'
 
-import Hero from '/components/homepage/Hero/Hero'
-import PresentacionEs from '/components/homepage/Presentacion/PresentacionEs'
-import EventosHomeEs from '/components/homepage/EventosHome/EventosHomeEs'
-import NuestrosServiciosEs from '/components/homepage/NuestrosServicios/NuestrosServiciosEs'
-import ConfiarEs from '/components/homepage/Confiar/ConfiarEs'
-import PilaresEs from '/components/homepage/Pilares/PilaresEs'
-import TestimoniosEs from '/components/homepage/Testimonios/TestimoniosEs'
-import NumerosEs from '/components/homepage/Numeros/NumerosEs'
-import ComoTrabajamosEs from '/components/homepage/ComoTrabajamos/ComoTrabajamosEs'
+import Hero from '/components/homepage/Hero/Hero';
+import PresentacionEs from '/components/homepage/Presentacion/PresentacionEs';
+import EventosHomeEs from '/components/homepage/EventosHome/EventosHomeEs';
+import NuestrosServiciosEs from '/components/homepage/NuestrosServicios/NuestrosServiciosEs';
+import ConfiarEs from '/components/homepage/Confiar/ConfiarEs';
+import PilaresEs from '/components/homepage/Pilares/PilaresEs';
+import TestimoniosEs from '/components/homepage/Testimonios/TestimoniosEs';
+import NumerosEs from '/components/homepage/Numeros/NumerosEs';
+import ComoTrabajamosEs from '/components/homepage/ComoTrabajamos/ComoTrabajamosEs';
 
-import ContactoEs from '/components/Contacto/ContactoEs'
-import Nav from '/components/Nav/Nav'
+import ContactoEs from '/components/Contacto/ContactoEs';
+import Nav from '/components/Nav/Nav';
+import NavEs from '../../components/Nav/NavEs';
 
 export default function Home() {
   // const value = useContext(AppContext);
@@ -31,17 +32,35 @@ export default function Home() {
   //   metadescription = 'Travel agency specialised in sports tourism and events. Sporting events and international concerts | Individual and group tailor-made trips.'
   // }
 
-
   return (
     <div className={styles.container1}>
       <Head>
-        <title>{'Polo & Wine | Agencia de Turismo Deportivo y Eventos'}</title>
-        <meta name="google-site-verification" content="UaSUKE7_aZwWN7sMTxwL1CWNkw4HIhiNrB6zHb07Mk4" />
-        <meta name="description" content={'Agencia de viajes especializada en turismo deportivo y eventos. Eventos Deportivos y conciertos internacionales | Viajes individuales y grupales a medida.'} />
-        <meta name="format-detection" content="telephone=no" />
-        <link rel="icon" href="/favicon.png" />
+        <title>
+          {
+            'Polo & Wine | Agencia de Turismo Deportivo y Eventos'
+          }
+        </title>
+        <meta
+          name="google-site-verification"
+          content="UaSUKE7_aZwWN7sMTxwL1CWNkw4HIhiNrB6zHb07Mk4"
+        />
+        <meta
+          name="description"
+          content={
+            'Agencia de viajes especializada en turismo deportivo y eventos. Eventos Deportivos y conciertos internacionales | Viajes individuales y grupales a medida.'
+          }
+        />
+        <meta
+          name="format-detection"
+          content="telephone=no"
+        />
+        <link
+          rel="icon"
+          href="/favicon.png"
+        />
       </Head>
-      <Nav languageObj={'es'} />
+      {/* <Nav languageObj={'es'} /> */}
+      <NavEs />
       <Hero languageObj={'es'} />
       <PresentacionEs />
       <EventosHomeEs />
@@ -54,8 +73,6 @@ export default function Home() {
       <NumerosEs />
       <ComoTrabajamosEs />
       <ContactoEs />
-
-
-    </div >
-  )
+    </div>
+  );
 }
